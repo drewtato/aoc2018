9d8ce2f882266a7e4be3f4624c31b5759732b193
drewtato <a.irino.42@gmail.com>
2018-11-30 21:15:05 -0800

    DONE DAY 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,2 +1,20 @@
 with open('input.txt', 'r') as input:
-    pass
+    freq = 0
+    newinput = input.read()
+    visited = set()
+    for line in newinput.split():
+        freq += int(line)
+    print(freq)
+    
+    going = True
+    freq = 0
+    while going:
+        for line in newinput.split():
+            freq += int(line)
+            if freq in visited:
+                going = False
+                print(freq)
+                break
+            else:
+                visited.add(freq)
+    

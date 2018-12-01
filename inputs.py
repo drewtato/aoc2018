708db622351ee3895dc2def3bae61f7c4f2134cf
drewtato <a.irino.42@gmail.com>
2018-11-30 16:51:33 -0800

    Setup space for Day 1, t-4h

---
+++ inputs.py
@@ -0,0 +1,39 @@
+import requests
+import sys
+import os
+import time
+
+day = sys.argv[1]
+
+go = '''
+            GGGGGGGGGGGGG         OOOOOOOOO     
+         GGG::::::::::::G       OO:::::::::OO   
+       GG:::::::::::::::G     OO:::::::::::::OO 
+      G:::::GGGGGGGG::::G    O:::::::OOO:::::::O
+     G:::::G       GGGGGG    O::::::O   O::::::O
+    G:::::G                  O:::::O     O:::::O
+    G:::::G                  O:::::O     O:::::O
+    G:::::G    GGGGGGGGGG    O:::::O     O:::::O
+    G:::::G    G::::::::G    O:::::O     O:::::O
+    G:::::G    GGGGG::::G    O:::::O     O:::::O
+    G:::::G        G::::G    O:::::O     O:::::O
+     G:::::G       G::::G    O::::::O   O::::::O
+      G:::::GGGGGGGG::::G    O:::::::OOO:::::::O
+       GG:::::::::::::::G     OO:::::::::::::OO 
+         GGG::::::GGG:::G       OO:::::::::OO   
+            GGGGGG   GGGG         OOOOOOOOO 
+'''
+
+with open('auth', 'r') as auth:
+    cookies = {'session': auth.read()}
+
+while not os.path.isfile(f'day{day}/input.txt'):
+    input = requests.get(f'https://adventofcode.com/2018/day/{day}/input', cookies=cookies)
+    if input.status_code != 200:
+        print(f'{input.status_code}, retry in 10')
+        time.sleep(10)
+    else:
+        print('200, saving file!')
+        print(go)
+        with open(f'day{day}/input.txt', 'wb') as f:
+            f.write(input.content)

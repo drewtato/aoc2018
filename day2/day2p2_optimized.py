241bf520a8538839f5db49e59566dd3ad828d21e
drewtato <a.irino.42@gmail.com>
2018-12-01 22:38:10 -0800

    Tried some other stuff to no avail

--- day2/day2p2_optimized.py
+++ day2/day2p2_optimized.py
@@ -11,3 +11,18 @@ with open('input.txt', 'r') as input:
                     seenWords.add(scrunched)
     except Exception:
         pass
+        
+    # # Me trying to get an answer using sorted lists: unsuccessful
+    # input = list(input)
+    # sortedFront = sorted(input)
+    # sortedBack = sorted([list(reversed(x)) for x in input])
+    
+    # for item,other in zip(sortedFront,sortedBack):
+    #     diffs = 0
+    #     for letter,otherLetter in zip(item,other):
+    #         if letter != otherLetter:
+    #             diffs += 1
+    #         elif diffs > 1:
+    #             break
+    #     if diffs == 1:
+    #         print(item, other)

014acb04dd099790b4e8b1be024eeb779579d7d1
drewtato <a.irino.42@gmail.com>
2018-12-02 21:38:21 -0800

    Finished in 38 minutes, not so impressive this time

---
+++ day3/day3.py
@@ -0,0 +1,54 @@
+import itertools
+
+LENGTH = 1000
+
+with open('input.txt', 'r') as input:
+    ans = 0
+    fabric = [0]*LENGTH*LENGTH
+    # print(fabric)
+    
+    rectangles = []
+    for line in input:
+        part = line\
+            .replace('#','')\
+            .replace(' @ ', ' ')\
+            .replace(',', ' ')\
+            .replace(': ', ' ')\
+            .replace('x', ' ')\
+            .split()
+        rectangles.append(list(map(int,part)))
+    # print(rectangles)
+    
+    for rect in rectangles:
+        x = rect[1]
+        y = rect[2]
+        for sx in range(rect[3]):
+            for sy in range(rect[4]):
+                index = x + sx + (y + sy)*LENGTH
+                fabric[index] += 1
+                
+                
+    for square in fabric:
+        if square > 1:
+            ans += 1
+
+    with open('fab.txt', 'w') as fab:
+        for i,square in enumerate(fabric):
+            fab.write(f'{square: 2}')
+            if not (i + 1)%LENGTH:
+                fab.write('\n')
+    print(ans)
+    
+    # Part 2
+    for rect in rectangles:
+        works = True
+        x = rect[1]
+        y = rect[2]
+        for sx in range(rect[3]):
+            for sy in range(rect[4]):
+                index = x + sx + (y + sy)*LENGTH
+                if fabric[index] - 1:
+                    works = False
+        if works:
+            print(rect[0])
+            break

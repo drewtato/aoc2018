4c26b32d7a57316ef8db55d999a651a01980f795
drewtato <a.irino.42@gmail.com>
2018-12-08 22:18:54 -0800

    Now works for both days

--- day09/day09.py
+++ day09/day09.py
@@ -11,27 +11,31 @@ with open('input.txt', 'r') as inp:
 
 inp = inp.split()
 numPlayers = int(inp[0])
-last = int(inp[6]) * 100
-# print(players, last)
-players = dd(int)
-currentPlayer = 0
-circle = dq([0])
+last = int(inp[6])
 
-for marble in range(1,last + 1):
-    currentPlayer = currentPlayer % numPlayers + 1
-    if marble % 23:
-        circle.append(circle.popleft())
-        circle.append(marble)
-    else:
-        players[currentPlayer] += marble
-        for _ in range(7):
-            circle.appendleft(circle.pop())
-        players[currentPlayer] += circle.pop()
-        circle.append(circle.popleft())
-        
-    # print(marble, currentPlayer, end='')
-    # for member in circle:
-    #     print(f' | {member}', end='')
-    # print()
+def play(numPlayers, last):
+    players = dd(int)
+    currentPlayer = 0
+    circle = dq([0])
 
-print(max(players.values()))
+    for marble in range(1,last + 1):
+        currentPlayer = currentPlayer % numPlayers + 1
+        if marble % 23:
+            circle.append(circle.popleft())
+            circle.append(marble)
+        else:
+            players[currentPlayer] += marble
+            for _ in range(7):
+                circle.appendleft(circle.pop())
+            players[currentPlayer] += circle.pop()
+            circle.append(circle.popleft())
+            
+        # print(marble, currentPlayer, end='')
+        # for member in circle:
+        #     print(f' | {member}', end='')
+        # print()
+
+    return max(players.values())
+
+print(play(numPlayers, last))
+print(play(numPlayers, last * 100))

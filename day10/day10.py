4fd1a78bf13c142baddc1badf4f804be45ad2bd5
drewtato <a.irino.42@gmail.com>
2018-12-10 00:41:19 -0800

    Optimization and created better image/text

--- day10/day10.py
+++ day10/day10.py
@@ -16,46 +16,72 @@ for line in inp.split('\n'):
     points.append([[int(line[10:16]), int(line[18:24])], [int(line[36:38]), int(line[40:42])]])
 
 # print(points)
-def getMinMax(points):
-    minposX = min(map(lambda point: point[0][0], points))
+def getChangeY(points):
     minposY = min(map(lambda point: point[0][1], points))
-    maxposX = max(map(lambda point: point[0][0], points))
     maxposY = max(map(lambda point: point[0][1], points))
-    return (maxposX - minposX, maxposY - minposY)
+    return maxposY - minposY
 
-thisMinMax = getMinMax(points)
+maxNegative = max(filter(lambda p: p[0][1] < 0, points), key=lambda p: abs(p[0][1] / p[1][1]))
+# print(maxNegative[0][1] / maxNegative[1][1])
+pre = abs(int(maxNegative[0][1] / maxNegative[1][1]))
+# print(pre)
+
+for p in points:
+    dx = p[1][0] * pre
+    dy = p[1][1] * pre
+    p[0] = [p[0][0] + dx, p[0][1] + dy]
+
+# print(points)
+thisY = getChangeY(points)
 # print(getMinMax(points))
 seconds = 0
-
-# It's gonna take a long time so might as well skip a few
-# SKIP = 1000
-
 
 while True:
     seconds += 1
     lastPoints = copy.deepcopy(points)
-    lastMinMax = thisMinMax
+    lastY = thisY
     for p in points:
         ax = p[1][0]
         ay = p[1][1]
         p[0][0] += ax
         p[0][1] += ay
-    thisMinMax = getMinMax(points)
-    # print(thisMinMax)
-    if thisMinMax > lastMinMax:
+    thisY = getChangeY(points)
+    # print(thisY)
+    if thisY > lastY:
         points = lastPoints
         seconds -= 1
         break
 
-thisMinMax = (thisMinMax[0] + 1, thisMinMax[1] + 1)
-img = Image.new('L', thisMinMax)
+# img = [[' '] * 200 for _ in range(200)]
+# for p in points:
+#     try:
+#         img[p[0][1]][p[0][0]] = '*'
+#     except IndexError as e:
+#         print(f'Indexes: {p[0]}')
+#         raise e
+    
+# string = ''
+# for line in img:
+#     for c in line:
+#         string += c
+#     string += '\n'
+
+# with open('stars.txt', 'w') as stars:
+#     stars.write(string)
+
+
+
 
 minposX = min(map(lambda p: p[0][0], points))
-minposY = min(map(lambda point: point[0][1], points))
+minposY = min(map(lambda p: p[0][1], points))
+# print(minposX, minposY)
+maxposX = max(map(lambda p: p[0][0], points))
+maxposY = max(map(lambda p: p[0][1], points))
 
+img = Image.new('L', (maxposX - minposX + 3, maxposY - minposY + 3))
 for p in points:
-    x = p[0][0] - minposX
-    y = p[0][1] - minposY
+    x = p[0][0] - minposX + 1
+    y = p[0][1] - minposY + 1
     img.putpixel((x,y), 255)
 
 # img.putpixel((0,0), 128)

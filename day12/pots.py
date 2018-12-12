ea3f1e897dc300135c650c7038c56f85db36e50d
drewtato <a.irino.42@gmail.com>
2018-12-11 23:48:43 -0800

    Done with visualization

---
+++ day12/pots.py
@@ -0,0 +1,64 @@
+from collections import deque as dq
+import itertools as it
+
+with open('input.txt', 'r') as inp:
+    inp = inp.read()
+
+while inp[-1].isspace():
+    inp = inp[:-1]
+
+pots = inp.split('\n')[0][15:]
+pots = dq(map(lambda p: 1 if p == '#' else 0, pots))
+# print(pots)
+
+rules = [0] * 2**5
+
+for line in reversed(sorted(inp.split('\n')[2:])):
+    # print(line)
+    if line[-1] == '#':
+        state = 0
+        for i in range(5):
+            state = state << 1
+            state += 1 if line[i] == '#' else 0
+        rules[state] = 1
+
+# print(rules)
+# print(len(pots))
+def generate(pots, rules, gens, f):
+    
+    def getsum(pots, zero):
+        s = 0
+        for i,p in zip(range(-zero, len(pots) - zero), pots):
+            if p:
+                s += i
+        return s
+                
+    zeroPot = 0
+    pots.extendleft(it.repeat(0, 2))
+    
+    for i in range(gens):
+        # print(i)
+        
+        for p in pots:
+            c = '#' if p else ' '
+            f.write(c)
+        f.write('\n')
+        
+        newpots = dq([0,0])
+        pots.extendleft([0,0])
+        pots.extend([0,0,0,0])
+        for i in range(len(pots) - 4):
+            state = 0
+            for j in range(5):
+                state = state << 1
+                state += pots[i + j]
+            newpots.append(rules[state])
+        while newpots.pop() == 0:
+            pass
+        newpots.append(1)
+        
+        pots = newpots
+    return getsum(pots, zeroPot)
+    
+with open('pots.txt', 'w') as f:
+    generate(pots, rules, 200, f)
---
+++ day12/visual.py
@@ -0,0 +1,50 @@
+from PIL import Image, ImageDraw, ImageFont
+import itertools as it
+
+GOLD = (255,255,102)
+BLUE = (15,15,35)
+RED = (255,0,0)
+GREEN = (0,153,0)
+GREY = (204,204,204)
+
+pots = []
+with open('pots.txt', 'r') as potsFile:
+    for line in potsFile:
+        pots.append([0 if c == ' ' else 1 for c in line])
+        
+# print(pots[1:4])
+
+maxLen = max([len(x) for x in pots])
+# print(maxLen)
+for line in pots:
+    line.extend(it.repeat(0, maxLen - len(line)))
+    
+data = []
+for i,line in enumerate(pots):
+    for j,p in enumerate(line):
+        if i == 20:
+            if j % 100 == 0:
+                data.append(RED if p else GREY)
+            else:
+                data.append(RED if p else BLUE)
+        else:
+            if j % 100 == 0:
+                data.append(GOLD if p else GREY)
+            else:
+                data.append(GOLD if p else BLUE)
+        
+sizeX = maxLen
+sizeY = len(pots)
+img = Image.new('RGB', (sizeX, sizeY))
+
+img.putdata(data)
+# img.show()
+img = img.resize((sizeX * 2, sizeY * 4))
+
+dr = ImageDraw.Draw(img)
+font = ImageFont.truetype('../assets/SourceCodePro-Light.ttf', 38)
+botleft = (78 * 4, 27 * 2 + 1)
+dr.rectangle([(78 * 4 + 40, 27 * 2 + 1), (500,105)], fill=BLUE)
+dr.text(botleft, '<- 3061', RED, font)
+
+img.save('pots.png')
